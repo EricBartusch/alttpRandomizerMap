96e12a6dacecf5b913516aa1bfe05565d1ed8d3d
Eric Bartusch <ericbartusch8@gmail.com>
2018-02-22 23:05:53 -0600

    1000 files baby

--- spoilerToDocuments.py
+++ spoilerToDocuments.py
@@ -1,20 +1,16 @@
 import json
+import os
 import createAttributeMap
 import getProgressiveItems
+from collections import OrderedDict
 
 def get_seed(spoiler_data):
     for section, value in spoiler_data.items():
         if(section == "meta"):
             return value["seed"]  
 
-SPOILER_DATA = json.load(open('testSpoiler.json'))
-SEED = get_seed(SPOILER_DATA)
-
-#Initialize constant information
-#TODO: Move this outside whatever loop is used to go through files
-PROGRESSIVE_ITEMS = getProgressiveItems.get_progressive_items(SPOILER_DATA)
+#Consistent information
 ATTRIBUTE_MAP = createAttributeMap.create_attribute_map()
-
 UNIQUE_ITEMS = ["Pegasus Boots", "Magic Powder", "Progessive Armor", "Lamp", "Bottle", "Magical Boomerang", "Bombos", \
  "Progressive Glove", "Moon Pearl", "Flippers", "Ice Rod", "Boomerang", "Half Magic", "Progressive Sword", "Cane of Byrna", \
  "Silver Arrows Upgrade", "Bug Catching Net", "Fire Rod", "Quake", "Book Of Mudora", "Progressive Shield", "Magic Mirror", "Magic Cape", \
@@ -29,18 +25,32 @@ NORMAL_ITEMS = ["Pice of Heart", "Twenty Rupees", "Three Hundred Rupees", "Three
 
 NON_DUNGEON_ITEMS = UNIQUE_ITEMS + OTHER_INTERESTING_ITEMS + NORMAL_ITEMS
 
-#Loop through all items and create documents for their data
-for section, value in SPOILER_DATA.items():
-    if(section != "Special" and section != "playthrough" and section != "meta" and section != "Castle Tower"):
-        for chest, item in value.items():
-            if(item in NON_DUNGEON_ITEMS):
-                document = {}
-                document["seed"] = SEED
-                document["item"] = item
-                document["location"] = chest
-                document["progression"] = item in PROGRESSIVE_ITEMS
-                document["unique"] = item in UNIQUE_ITEMS
-                document["attributes"] = ATTRIBUTE_MAP[chest]
-                chestTrimmed = chest.replace("'", "").replace(" ", "")
-                with open("documents/" + str(SEED) + "-" + chestTrimmed + ".json", "w", encoding='utf-8') as doc:
-                    json.dump(document, doc, sort_keys=True, indent=4)
+for filename in os.listdir("spoilers"):
+    #seed dependent info
+    SPOILER_DATA = json.load(open("spoilers/" + filename))
+    PROGRESSIVE_ITEMS = getProgressiveItems.get_progressive_items(SPOILER_DATA)
+    SEED = get_seed(SPOILER_DATA)
+
+
+    #Loop through all items and create documents for their data
+    id = 1
+    for section, value in SPOILER_DATA.items():
+        if(section != "Special" and section != "playthrough" and section != "meta" and section != "Castle Tower"):
+            for chest, item in sorted(value.items()):
+                if(item in NON_DUNGEON_ITEMS):
+                    indexId = {"index" : {"_id" : id}}
+                    document = {}
+                    document["seed"] = SEED
+                    document["item"] = item
+                    document["location"] = chest
+                    document["progression"] = item in PROGRESSIVE_ITEMS
+                    document["unique"] = item in UNIQUE_ITEMS
+                    document["attrs"] = ATTRIBUTE_MAP[chest]
+                    orderedDocument = OrderedDict(document)
+                    id += 1
+                    with open("documents/" + str(SEED) + ".json", "a", encoding='utf-8') as doc:
+                        json.dump(indexId, doc)
+                        doc.write('\n')
+                        json.dump(orderedDocument, doc)
+                        doc.write('\n')
+                        doc.close()
