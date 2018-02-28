39cf64f0fd516d67c85211465c0dfce77e8a1bfd
Eric Bartusch <ericbartusch8@gmail.com>
2018-02-27 21:20:04 -0600

    Fixing item names

--- spoilerToDocuments.py
+++ spoilerToDocuments.py
@@ -22,19 +22,21 @@ doDungeonItems = int(args.doDungeonItems)
     
 #Consistent information
 ATTRIBUTE_MAP = createAttributeMap.create_attribute_map()
-UNIQUE_ITEMS = ["Pegasus Boots", "Magic Powder", "Progessive Armor", "Lamp", "Bottle", "Magical Boomerang", "Bombos", \
- "Progressive Glove", "Moon Pearl", "Flippers", "Ice Rod", "Boomerang", "Half Magic", "Progressive Sword", "Cane of Byrna", \
+UNIQUE_ITEMS = ["Pegasus Boots", "Magic Powder", "Progressive Armor", "Lamp", "Bottle (Empty)", "Magical Boomerang", "Bombos", \
+ "Progressive Glove", "Moon Pearl", "Flippers", "Ice Rod", "Boomerang", "Half Magic", "Progressive Sword", "Cane Of Byrna", \
  "Silver Arrows Upgrade", "Bug Catching Net", "Fire Rod", "Quake", "Book Of Mudora", "Progressive Shield", "Magic Mirror", "Magic Cape", \
- "Cane of Somaria", "Flute", "Ether", "Bow", "Hookshot", "Shovel", "Mushroom", "Bottle (Bee)", "Bottle (Golden Bee)", "Bottle (Red Potion)", \
+ "Cane Of Somaria", "Flute", "Ether", "Bow", "Hookshot", "Shovel", "Mushroom", "Bottle (Bee)", "Bottle (Golden Bee)", "Bottle (Red Potion)", \
  "Bottle (Green Potion)", "Bottle (Blue Potion)"]
 
 #items people point out but aren't inheriently special
 OTHER_INTERESTING_ITEMS = ["Heart Container (refill)", "One Rupee", "Arrow"]
 
-NORMAL_ITEMS = ["Pice of Heart", "Twenty Rupees", "Three Hundred Rupees", "Three Bombs", "Five Rupees", "Fifty Rupees", "Arrow Upgrade (5)", \
-"Ten Arrows", "Bomb Upgrade (10)", "Arrow Upgrade (10)", "Bomb Upgrade (5)"]
+NORMAL_ITEMS = ["Piece Of Heart", "Heart Container", "Twenty Rupees", "Three Hundred Rupees", "Three Bombs", "Five Rupees", "Fifty Rupees", "Arrow Upgrade (5)", \
+"Ten Arrows", "Bomb Upgrade (10)", "Arrow Upgrade (10)", "Bomb Upgrade (5)", "One Hundred Rupees"]
 
 NON_DUNGEON_ITEMS = UNIQUE_ITEMS + OTHER_INTERESTING_ITEMS + NORMAL_ITEMS
+
+DUNGEON_ITEMS = [""]
 
 #Elastic ID - needs to be unique for all items
 for filename in os.listdir("spoilers"):
@@ -84,7 +86,7 @@ for filename in os.listdir("spoilers"):
                             document["attrs"].append("Big Key")
                         orderedDocument = OrderedDict(document)
                         with open("dungeonDocs/" + str(SEED) + "-dungeon.json", "a", encoding='utf-8') as doc:
-                            json.dump(dungeonIndexId, doc)
+                            json.dump(indexId, doc)
                             doc.write('\n')
                             json.dump(orderedDocument, doc)
                             doc.write('\n')
